a363d29b28f13797d9c66ad6811fa8633bb63393
Ademola Badmus <btinfo.demola@gmail.com>
2024-06-02 22:56:04 +0100

    first commit

---
+++ .storybook/preview.js
@@ -0,0 +1,21 @@
+import "../src/index.css";
+import { initialize, mswLoader } from "msw-storybook-addon";
+
+// Initialize MSW
+initialize();
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  loaders: [mswLoader],
+};
+
+export default preview;
